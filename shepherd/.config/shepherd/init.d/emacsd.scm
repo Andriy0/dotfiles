c8469c262042935cc6895d44f96439b1a0ad598d
Andriy0 <andriy2152@gmail.com>
2021-06-18 12:57:32 +0300

    Renamed emacs-daemon to emacsd

--- shepherd/.config/shepherd/init.d/emacsd.scm
+++ shepherd/.config/shepherd/init.d/emacsd.scm
@@ -1,11 +1,12 @@
-(define emacs-daemon
+(define emacsd
   (make <service>
     #:declarative? #f
-    #:provides '(emacs-daemon)
+    #:provides '(emacsd)
     #:docstring "Run `emacs' as daemon"
     #:start (make-forkexec-constructor '("emacs" "--fg-daemon"))
+    ;; "-l ~/dotfiles/emacs/.config/emacs/desktop.el" 
     #:stop (make-kill-destructor)
     #:respawn? #t))
 
-(register-services emacs-daemon)
-(start emacs-daemon)
+(register-services emacsd)
+(start emacsd)
