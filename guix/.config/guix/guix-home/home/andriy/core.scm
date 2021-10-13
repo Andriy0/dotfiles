e28d266796abedc9d1448b4b3af9b8482b9d4491
Andriy0 <andriy2152@gmail.com>
2021-10-14 01:28:51 +0300

    Added guix-home config.

---
+++ guix/.config/guix/guix-home/home/andriy/core.scm
@@ -0,0 +1,31 @@
+(define-module (home andriy core)
+  #:use-module (gnu home)
+  ;; #:use-module ((home yggdrasil gnupg) #:prefix gnupg:)
+  ;; #:use-module ((home yggdrasil version-control) #:prefix vc:)
+  ;; #:use-module ((home yggdrasil wm) #:prefix wm:)
+  ;; #:use-module ((home yggdrasil emacs) #:prefix emacs:)
+  #:use-module ((home andriy shell) #:prefix shell:)
+  ;; #:use-module ((home yggdrasil password-utils) #:prefix pass:)
+  ;; #:use-module ((home yggdrasil xdg) #:prefix xdg:)
+  ;; #:use-module ((home yggdrasil ssh) #:prefix ssh:)
+  #:use-module ((home andriy pipewire) #:prefix pw:)
+  ;; #:use-module ((home yggdrasil terminals) #:prefix term:)
+  ;; #:use-module ((home yggdrasil mail) #:prefix mail:)
+  #:use-module ((home andriy packages) #:select (packages)))
+
+(home-environment
+ (packages packages)
+ (services
+  (append
+   ;; wm:services
+   ;; vc:services
+   ;; gnupg:services
+   ;; emacs:services
+   ;; pass:services
+   shell:services
+   ;; xdg:services
+   ;; ssh:services
+   pw:services
+   ;; term:services
+   ;; mail:services
+   )))
---
+++ guix/.config/guix/guix-home/home/andriy/pipewire.scm
@@ -0,0 +1,9 @@
+(define-module (home andriy pipewire)
+  #:use-module (gnu home-services)
+  ;; #:use-module (kreved home-services dbus)
+  #:use-module (home andriy pipewire-service))
+
+(define-public services
+  (list
+   ;; (service home-dbus-service-type)
+   (service home-pipewire-service-type)))
